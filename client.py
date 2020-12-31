98c1d41d704560f18fa2d20e329d16171c928ab0
shimonyg <shimonyg@post.bgu.ac.il>
2020-12-31 15:11:18 +0200

    client class revised

--- client.py
+++ client.py
@@ -13,99 +13,124 @@ except ModuleNotFoundError:
     #USED FOR WINDOWS
     import msvcrt
 
-def run_game():
+class GameClient:
 
-    #Game runs for 10 seconds
-    now = time.time()
-    future = now + 10
+    def __init__(self):
+        # Listen for broadcast of UDP from the server
+        print('Client started, listening for offer requests...')
+        self.client_offer_socket = self.start_listening()
+        self.client_tcp_socket = None
 
-    char =""
-    while time.time() < future:
-        #  START CATCHING KEYBOARD HIT FROM THE USER
-        try:
-            #################
-            #  USED FOR LINUX
+    def run_game(self):
+
+        #Game runs for 10 seconds
+        now = time.time()
+        future = now + 10
+
+        char =""
+        while time.time() < future:
+            #  START CATCHING KEYBOARD HIT FROM THE USER
             try:
-                char = getch.getch()
-                client.sendall(char.encode('utf-8'))
-            ####################
-
-            except NameError:
                 #################
-                #  USED FOR WINDOWS
-                char = msvcrt.getch()
-                client.sendall(char)
+                #  USED FOR LINUX
+                try:
+                    char = getch.getch()
+                    self.client_tcp_socket.sendall(char.encode('utf-8'))
                 ####################
 
-        except (ConnectionResetError , TimeoutError , OSError):
-            print("CONNECTION ERROR")
-            return None
+                except NameError:
+                    #################
+                    #  USED FOR WINDOWS
+                    char = msvcrt.getch()
+                    self.client_tcp_socket.sendall(char)
+                    ####################
 
-# Listen for broadcast of UDP from the server
-print('Client started, listening for offer requests...')
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+            except (ConnectionResetError , TimeoutError , OSError):
+                print("CONNECTION ERROR")
+                #exit the game -> handler will catch in the calling function
+                return None
 
-# Enable broadcasting mode
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-client.bind(("", SERVER_DEST_PORT))
-while True:
-    try:
-        data, addr = None, None
-        while not data:
-            data, addr = client.recvfrom(1024)
-
-        if len(data) < PAYLOAD_SIZE:
-            if not data:
-                # client closed after last message, assume all is well
-                print("Client Closed")
-                continue
-            else:
-                # raise IOError("Truncated message")
-                print("Truncated message")
-                continue
-
-        #check if the data received is indeed the offer packet desired
-        if not OfferPacket.validate_packet(data):
-            print("Recieved a message which is not an offer message. Please try sending again")
-            continue
-
-        port = OfferPacket.get_port_from_data(data)
-        print(f"Received offer from {addr[0]}, attempting to connect...")
-        client = socket.socket(); 
-        client.connect((addr[0], port))
-        print("Connection Established")
-        
-
-        #establish my team name and send it to the server
-        client.sendall(TEAM_NAME.encode('utf-8'))
-
-        #get the welcome message from the server and print to the screen
-        welcome_message = client.recv(1024*4).decode("ASCII")
-        print(welcome_message)
-
-        client.settimeout(50)
-
-        #run the game and press as many keys as you can
-        run_game()
-
-        #get the end message from the server for the score outcomes and print to the screen
-        while True:
-            end_messages = client.recv(1024*4).decode("ASCII")
-            if not end_messages:
-                time.sleep(5)
-                client.close()
-                break
-            print(end_messages)
-            
-    except (ConnectionResetError , TimeoutError , OSError, ConnectionRefusedError) as e:
-        print("Server disconnected, listening for offer requests...")
+    def start_listening(self):
+        #creating new socket for the udp connection
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+
         # Enable broadcasting mode
         client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
         client.bind(("", SERVER_DEST_PORT))
-        continue
- 
-    except socket.timeout:
-        print("Connection time out")
-        continue
+        return client
+
+    def get_game_details(self): 
+
+            data, addr = None, None
+            while not data:
+                data, addr = self.client_offer_socket.recvfrom(1024)
+
+            if len(data) < PAYLOAD_SIZE:
+                if not data:
+                    # client closed after last message, assume all is well
+                    print("Client Closed")
+                    return None, 0
+                else:
+                    # raise IOError("Truncated message")
+                    print("Truncated message")
+                    return None, 0 
+
+            #check if the data received is indeed the offer packet desired
+            if not OfferPacket.validate_packet(data):
+                print("Recieved a message which is not an offer message. Please try sending again")
+                return None, 0
+
+            port = OfferPacket.get_port_from_data(data)
+            return port, addr
+        
+
+    def game_handler(self):
+
+        while True:
+            try:
+                port, addr = self.get_game_details()
+                if port == None:
+                    continue
+                #connect to the sever with the given port
+                print(f"Received offer from {addr[0]}, attempting to connect...")
+                self.client_tcp_socket = socket.socket(); 
+                self.client_tcp_socket.connect((addr[0], port))
+                print("Connection Established")
+                
+                #establish my team name and send it to the server
+                self.client_tcp_socket.sendall(TEAM_NAME.encode('utf-8'))
+
+                #get the welcome message from the server and print to the screen
+                welcome_message = self.client_tcp_socket.recv(1024*4).decode("ASCII")
+                print(welcome_message)
+
+                self.client_tcp_socket.settimeout(50)
+
+                #run the game and press as many keys as you can
+                client.run_game()
+
+                #get the end message from the server for the score outcomes and print to the screen
+                while True:
+                    end_messages = self.client_tcp_socket.recv(1024*4).decode("ASCII")
+                    if not end_messages:
+                        time.sleep(5)
+                        #game has ended, server stopped sending messages -> close the connection
+                        client.close_connection()
+                        break
+                    print(end_messages)
+                
+            except (ConnectionResetError , TimeoutError , OSError, ConnectionRefusedError):                
+                print("Server disconnected, listening for offer requests...")
+                self.client_offer_socket = self.start_listening()
+                continue
+        
+            except socket.timeout:
+                print("Connection time out, trying to reconnect..")
+                continue
+
+    def close_connection(self):
+        self.client_tcp_socket.close()
+
+client = GameClient()
+client.game_handler()
